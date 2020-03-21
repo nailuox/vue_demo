682acc0b0dff9d1b3e646060f0ed244fde03fd31
xiaqiudong <xiaqiudong@ahkeli.com>
2020-03-21 14:15:21 +0800

    fix build file public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 // antd 自定义主题
 module.exports = {
+	// 相对路径 ('./')，这样所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	css: {
 		loaderOptions: {
 			less: {
@@ -13,12 +15,13 @@ module.exports = {
 		}
 	},
 	devServer: {
-    // port: 7788,
-    proxy: {
-      '/data': {
-        changeOrigin: true,//允许跨域
-        target: 'http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10'
-      }
-    }
-  }
+		// port: 7788,
+		proxy: {
+			'/data': {
+				changeOrigin: true, //允许跨域
+				target:
+					'http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10'
+			}
+		}
+	}
 }
